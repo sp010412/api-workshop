fd95630da63f4f6f9d02e53e99d31a8326beaf3a
sp010412 <smajwede7@gmail.com>
2021-11-26 16:42:16 +0200

    eslint

--- public/app.js
+++ public/app.js
@@ -1,27 +1,26 @@
-var carColors = document.querySelector(".colors");
-var allCars = document.querySelector(".cars");
-var carMakes = document.querySelector(".brands");
+/* eslint-disable no-undef */
+var carColors = document.querySelector('.colors');
+var allCars = document.querySelector('.cars');
+var carMakes = document.querySelector('.brands');
 
-var templateSource = document.querySelector(".userTemplate");
+var templateSource = document.querySelector('.userTemplate');
 var userTemplate = Handlebars.compile(templateSource.innerHTML);
 
-
-
-axios.get("https://api-tutor.herokuapp.com/v1/colors")
+axios.get('https://api-tutor.herokuapp.com/v1/colors')
     .then(function (element) {
         carColors.innerHTML = userTemplate({
             colors: element.data
         });
     });
 
-    axios.get("https://api-tutor.herokuapp.com/v1/makes")
+axios.get('https://api-tutor.herokuapp.com/v1/makes')
     .then(function (element) {
         carMakes.innerHTML = userTemplate({
             colors: element.data
         });
     });
 
-    axios.get("https://api-tutor.herokuapp.com/v1/cars")
+axios.get('http://api-tutor.herokuapp.com/v1/cars')
     .then(function (element) {
         allCars.innerHTML = userTemplate({
             cars: element.data
--- public/app2.js
+++ public/app2.js
@@ -1,34 +1,29 @@
-var show = document.querySelector(".display");
-var btn = document.querySelector(".show");
+/* eslint-disable no-undef */
+let show = document.querySelector('.display');
+let btn = document.querySelector('.show');
 
-var templateSource = document.querySelector(".userTemplate2");
+var templateSource = document.querySelector('.userTemplate2');
 var userTemplate2 = Handlebars.compile(templateSource.innerHTML);
 
 btn.addEventListener('click', function () {
-
-    var colors = document.querySelector(".byColor").value;
-    var makes = document.querySelector(".byMake").value;
+    var colors = document.querySelector('.byColor').value;
+    var makes = document.querySelector('.byMake').value;
 
     if (colors && !makes) {
-
         axios.get(`https://api-tutor.herokuapp.com/v1/cars/color/${colors}`)
             .then(function (element) {
                 show.innerHTML = userTemplate2({
                     all: element.data
                 });
             });
-
     } else if (makes && !colors) {
-
         axios.get(`https://api-tutor.herokuapp.com/v1/cars/make/${makes}`)
             .then(function (element) {
                 show.innerHTML = userTemplate2({
                     all: element.data
                 });
             });
-
     } else if (colors && makes) {
-
         axios.get(`https://api-tutor.herokuapp.com/v1/cars/make/${makes}/color/${colors}`)
             .then(function (element) {
                 show.innerHTML = userTemplate2({
@@ -36,5 +31,4 @@ btn.addEventListener('click', function () {
                 });
             });
     }
-
 });
